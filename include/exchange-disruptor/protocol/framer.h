9558c8fd5744a017fa07d3c520dfca63778a56a5
benjamin Emily <benji.emily@icloud.com>
2026-02-17 14:11:44 +0100

    end framing

--- include/exchange-disruptor/protocol/framer.h
+++ include/exchange-disruptor/protocol/framer.h
@@ -10,6 +10,10 @@ public:
 
     std::vector<std::uint8_t> frame(const std::vector<std::uint8_t>& payloadBytes);
     std::vector<std::vector<uint8_t>> consume(const std::vector<std::uint8_t>& buffer);
+private:
+    std::vector<uint8_t> _buffer;
+    std::size_t _headerPos = 0;
+    static constexpr std::size_t kHeaderSize = 4;
 };
 
 #endif // FRAMER_H
--- src/protocol/framer.cpp
+++ src/protocol/framer.cpp
@@ -4,19 +4,42 @@
 
 Framer::Framer() {}
 
-std::vector<std::uint8_t> frame(const std::vector<std::uint8_t>& payloadBytes) { 
+std::vector<std::uint8_t> Framer::frame(const std::vector<std::uint8_t>& payloadBytes) { 
     uint32_t len = payloadBytes.size();
     std::vector<std::uint8_t> framedBytes;
     framedBytes.resize(4 + len);
     //big endian (network byte order)
-    framedBytes[0] = static_cast<std::uint8_t>(len & 0xFF);
-    framedBytes[1] = static_cast<std::uint8_t>((len >> 8) & 0xFF);
-    framedBytes[2] = static_cast<std::uint8_t>((len >> 16) & 0xFF);
-    framedBytes[3] = static_cast<std::uint8_t>((len >> 24) & 0xFF);
+    framedBytes[0] = static_cast<uint8_t>((len >> 24) & 0xFF);
+    framedBytes[1] = static_cast<uint8_t>((len >> 16) & 0xFF);
+    framedBytes[2] = static_cast<uint8_t>((len >> 8) & 0xFF);
+    framedBytes[3] = static_cast<uint8_t>( len & 0xFF);
     std::copy(payloadBytes.begin(), payloadBytes.end(), framedBytes.begin() + 4);
     return framedBytes;
 }
 
-std::vector<std::vector<uint8_t>> consume(const std::vector<std::uint8_t>& buffer) {
-    return std::vector<std::vector<uint8_t>>{};
+std::vector<std::vector<uint8_t>> Framer::consume(const std::vector<std::uint8_t>& buffer) {
+    _buffer.insert(_buffer.end(), buffer.begin(), buffer.end());
+    std::vector<std::vector<uint8_t>> messages;
+    while (_buffer.size() - _headerPos >= kHeaderSize) {
+        uint32_t len = (uint32_t(_buffer[_headerPos + 0]) << 24) | (uint32_t(_buffer[_headerPos + 1]) << 16) | (uint32_t(_buffer[_headerPos + 2]) <<  8) | (uint32_t(_buffer[_headerPos + 3]));
+        if (_buffer.size() - _headerPos < kHeaderSize + len) {
+            break;
+        }
+        if (len >= 1024) { //bad frame size
+            break;
+        }
+        std::vector<uint8_t> message(len);
+        std::copy(_buffer.begin() + _headerPos + kHeaderSize, _buffer.begin() + _headerPos + kHeaderSize + len, message.begin());
+        _headerPos += kHeaderSize + len;  
+        if (_headerPos == _buffer.size()) {
+            _headerPos = 0;
+            _buffer.clear();
+        }  
+        if (_headerPos > _buffer.size()/2) {
+            _buffer.erase(_buffer.begin(), _buffer.begin() + _headerPos);
+            _headerPos = 0;
+        }
+        messages.push_back(message);
+    }
+    return messages;
 }
